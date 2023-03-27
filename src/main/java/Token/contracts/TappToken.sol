9cdc5f6e2cfd5e80940561b50583010a36c45417
G00387170 <G00387170@atu.ie>
2023-03-27 18:24:32 +0100

    Finished delegated transfers - probably won't include in final code

--- src/main/java/Token/contracts/TappToken.sol
+++ src/main/java/Token/contracts/TappToken.sol
@@ -19,10 +19,21 @@ contract TappToken {
         uint256 _value
     );
 
+    //Declare Approve event - ways in solidity to admit something happened
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     //Define Mapping - give key and value (kinda like an array)
     // key is address and value is unsigned int - Map is called balanceOf
     //Default to 0 if not specified in code
     mapping(address => uint256) public balanceOf;
+
+    //Define the allowance for the approval function
+    //Nested mapping - read first address Account A is approving second address Acount B,C,D etc
+    mapping(address => mapping(address => uint256)) public allowance;
 
     //Constructor Function
     //Used underscore in initialSupply due to solidity conventions
@@ -54,14 +65,38 @@ contract TappToken {
     }
 
     //***Delegated Transfers (ERC 20)***
-    //transferFrom
     //Approved function
     //Approve this account to spend a certain amount of tokens
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        //allowance
+        //allowance - how much account can spend
+        allowance[msg.sender][_spender] = _value;
+
         //approved event
+        Approval(msg.sender, _spender, _value);
+
         return true;
 
+    }
+
+    //transferFrom - Handle the delegated transfer - execution
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        
+        //Add requirements for transfer: _from has enough tokens, allowance is big enough
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        //Change balanceOf
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        //Update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        //call transfer event
+        Transfer(_from, _to, _value);
+
+        //return a boolean
+        return true;
     }
 
 }
