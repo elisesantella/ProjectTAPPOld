e9cc160e5e8ac2a20639b527fe65b62611168d7c
G00387170 <G00387170@atu.ie>
2023-04-06 18:04:27 +0100

    Finished the sales function and set on building a front end

--- src/main/java/Token/contracts/TappTokenSale.sol
+++ src/main/java/Token/contracts/TappTokenSale.sol
@@ -20,16 +20,42 @@ contract TappTokenSale {
         tokenPrice = _tokenPrice;
     }
 
+    //Create a multiply function - reference an external library
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
     //Buy tokens, pass in number of tokens they want, payable so can send it in ether??
     function buyTokens(uint256 _numberOfTokens) public payable {
+
         //Require that price number of tokens are the same as token price 
         require(msg.value == _numberOfTokens * tokenPrice);
-        //Require that there are enough tokens in the contract 
+
+        //Require that there are enough tokens in the contract, this points token contract
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+
         //Require transfer is successful
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));  
+        
         //Keep track of number of tokens sold
         tokenSold += _numberOfTokens;
+
         //Trigger Seller Event
         Sell(msg.sender, _numberOfTokens);
+    }
+
+    //Ending the token sale
+    function endSale() public {
+
+        //require that only admin can end sale
+        require(msg.sender == admin);
+
+        //when sale is ended - transfer the amount of tokens in sale remaining back to admin
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
+
+        //transfer balance to admin
+        //selfdestruct(admin);
+        admin.transfer(address(this).balance);
     }
 
 
--- src/main/java/Token/test/TappTokenSale.js
+++ src/main/java/Token/test/TappTokenSale.js
@@ -1,9 +1,13 @@
 var TappTokenSale = artifacts.require("./TappTokenSale.sol");
+var TappToken = artifacts.require("./TappToken.sol");
 
 contract('TappTokenSale', function(accounts) {
     var tokenSaleInstance;
+    var tokenInstance;
+    var admin = accounts[0];
     var buyer = accounts[1];
     var tokenPrice = 1000000000000000; //in wei - solidity
+    var tokensAvaliable = 750000;
     var numberOfTokens;
 
     it('initalises the contract with the correct values', function() {
@@ -22,8 +26,16 @@ contract('TappTokenSale', function(accounts) {
     });
 
     it('faciliates token buying', function() {
-        return TappTokenSale.deployed().then(function(instance) {
+        return TappToken.deployed().then(function(instance) {
+            //grab token instance first
+            tokenInstance = instance;
+            return TappTokenSale.deployed();
+        }).then(function(instance) {
+            //then grab token sale instance
             tokenSaleInstance = instance;
+            //provision 75% of all tokens to the token sale
+            return tokenInstance.transfer(tokenSaleInstance.address, tokensAvaliable, { from: admin })
+        }).then(function(receipt) {
             numberOfTokens = 10;
             return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice })
         }).then(function(receipt) {
@@ -34,12 +46,46 @@ contract('TappTokenSale', function(accounts) {
             return tokenSaleInstance.tokenSold();
         }).then(function(amount) {
             assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
-            //Try buy tokens different from ether value
+            return tokenInstance.balanceOf(buyer);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), numberOfTokens);
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), tokensAvaliable - numberOfTokens);
+            //Try buy tokens different from ether value;
             return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1 });
         }).then(assert.fail).catch(function(error) {
             assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+            return tokenSaleInstance.buyTokens(800000, { from: buyer, value: numberOfTokens * tokenPrice})
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than avaliable');
         });
     });
+
+    it('ends token sale', function() {
+        return TappToken.deployed().then(function(instance) {
+          // Grab token instance first
+          tokenInstance = instance;
+          return TappTokenSale.deployed();
+        }).then(function(instance) {
+          // Then grab token sale instance
+          tokenSaleInstance = instance;
+          // Try to end sale from account other than the admin
+          return tokenSaleInstance.endSale({ from: buyer });
+        }).then(assert.fail).catch(function(error) {
+          assert(error.message.indexOf('revert' >= 0, 'must be admin to end sale'));
+          // End sale as admin
+          return tokenSaleInstance.endSale({ from: admin });
+        }).then(function(receipt) {
+          return tokenInstance.balanceOf(admin);
+        }).then(function(balance) {
+          assert.equal(balance.toNumber(), 999990, 'returns all unsold dapp tokens to admin');
+          // Check that the contract has no balance -BELOW NOT WORKING WHTY
+          return balance = web3.eth.getBalance(tokenSaleInstance.address);
+        }).then(function(balance) {
+          assert.equal(balance, 0);
+        });
+      });
 
 
 
